1bd35f708417fe4a706e4fd5f6144fac61fe4489
nijhoomroy <56860479+nijhoomroy@users.noreply.github.com>
2024-04-15 20:43:45 -0500

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { typeDefs } from "./schema"
+
+async function startApolloServer() {
+    const server = new ApolloServer({ typeDefs });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+    Server is running!
+    Query at ${url}`)
+}
+
+startApolloServer();
